603bc9a55af4abe40d7e923d32412696db060a0c
andresathler <andremoriya.sathler@gmail.com>
2022-09-10 16:40:56 -0300

    commit monster-rolodex

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+import './search-box.styles.css';
+
+class SearchBox extends Component {
+
+    render() {
+        const { className, onChangeHandler, placeholder } = this.props;
+
+        return (
+            <div>
+                <input
+                    className={`search-box ${className}`}
+                    type='search'
+                    placeholder={placeholder}
+                    onChange={onChangeHandler}
+                />
+            </div>
+        )
+    }
+}
+
+export default SearchBox;
